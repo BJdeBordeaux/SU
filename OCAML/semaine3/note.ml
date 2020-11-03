ac489cfd18cfb9d3a969ab1c34a2026afa0da315
Junji <qq870185814@163.com>
2020-11-03 17:02:16 +0100

    ocaml cs4 update

--- OCAML/semaine3/note.ml
+++ OCAML/semaine3/note.ml
@@ -32,6 +32,25 @@ let app2 (la : 'a list) (lb: 'a list) : 'a list =
 		| x :: xs -> x :: (loop xs r)
 	in loop la lb
 
+let rec mem (a: 'a) (l: 'a list) : bool = 
+	match l with 
+	|[] -> false
+	|x :: l2 -> (a=x) || mem a l2
+
+let _ = assert(mem 5 [] = false)
+let _ = assert(mem 5 [5] = true)
+let _ = assert(mem 5 [1; 4; 5] = true)
+let _ = assert(mem 5 [1; 4; 3] = false)
+
+let nth (l: 'a list) (n: int) : 'a = 
+	let rec loop (l: 'a list) (n: int) = 
+		match l with 
+		|[] -> raise(Failure "nth")
+		|x :: ls -> if(n=0)then x else loop ls (n-1)
+	in if(n<0)then raise(Invalid_argument "n can not be less than 0")
+	else loop l n
+	
+
 let rec rev (l: 'a list) : 'a list = 
 	match l with 
 	| [] -> l
