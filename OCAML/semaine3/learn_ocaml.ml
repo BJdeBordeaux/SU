aac80dce41992d6ef530ddcc0ee9cc4fc322e0b2
Junji <qq870185814@163.com>
2020-11-22 09:59:12 +0100

    update learn ocaml

--- OCAML/semaine3/learn_ocaml.ml
+++ OCAML/semaine3/learn_ocaml.ml
@@ -82,6 +82,35 @@ let rec range_n (x:int) (n:int) : (int list) =
 
   (* list_scheme_red *)
 
+  let rec prod (xs:float list) : float =
+    match xs with
+    |[] -> 1.0 
+    |x :: l -> x *. prod l
+  
+  let rec sum_round (xs:float list) : int =
+    match xs with
+    |[] -> 0
+    |x :: l -> (int_of_float x) + sum_round l
+  
+  let rec parenthese (xs:string list) : string =
+    match xs with
+    |[] -> ""
+    |x :: l -> "("^x^")"^(parenthese l)
+                         
+  let rec flatten (xss:('a list) list) : 'a list =
+    match xss with
+    |[] -> []
+    |xs :: l -> xs @ flatten l
+  
+  let rec sum_tuple (cs:(int*int) list) : int =
+    match cs with
+    |[] -> 0
+    |(x, y) :: l -> x + y + sum_tuple l
+  
+  let rec reduce (f:'a -> 'b -> 'b) (xs:'a list) (b:'b) : 'b =
+    match xs with
+    |[] -> b
+    |x :: l -> f x (reduce f l b)
 
   (* merge_sort *)
   (* Q1 *)
