0ab5b6e32c6fbfffcd0d1d5a9354949cfe12cad8
BJdeBordeaux <qq870185814@163.com>
2022-02-15 22:16:27 +0100

    tme update

---
+++ technoWeb/monbirdy/src/Logout.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Login extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+          <div></div>
+        );
+    }
+}
+
+export default Login;
