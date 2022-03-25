c1dfc697a5df0ae76ba0afda5c83d49d0bb2fde0
BJdeBordeaux <qq870185814@163.com>
2022-03-25 17:27:15 +0100

    tme5-6 update

--- fondementOS/TME5-6/README.md
+++ fondementOS/TME5-6/README.md
@@ -6,3 +6,35 @@
     1. Sémaphore SEM_NB_RECEPT (voir le code)
     1. Ce tableau assure qu'un Recepteur ne peut que prendre le même message une seule fois ; s'il y a qu'une seul sémaphore, il se peut que le même Recepteur fait V (tenter de décrementer le nb_recepteur plusieures fois). Sémaphore SEM_EMET previent les émetteurs de la disponibilité de la case.
     1. Voir le code.
+1. TabNcase
+    1. 
+        1. Variables (valeur initiale): `indexEcrireCase (0)` pour la case qu'on va écrire le message, `nb_recepteurs[NMAX] (0)` pour compter combien de récepteur a reçu le message de cette case, `caseContenu[NMAX] (-1)` pour le contenu d'une case.
+        1. Sémaphores (valeur initiale): `S_EMETTEUR (NMAX)`  pour limiter droit d'écriture, `S_IndexEcrireCase (1)` pour progéger `indexEcrireCase`, `S_NbRecption (1)` pour protéger `nb_recepteurs[NMAX]`, `S_CASES[NMAX] (1)` pour protéger `caseContenu[NMAX]`, `S_RECEPTEUR[NR] (0)` pour éviter un récepteur de recevoir une case non écrite. (voir le code)
+    1.  côté récepteur:
+        ```python3
+            P(S_RECEPTEUR[id_recp]) # demander le droit de lecture
+            P(S_CASES[caseALire]) # monopoliser la case à lire
+                lire(caseALire) # lire la case
+            V(S_CASES[caseALire]) # libérer ce monopole
+            P(S_NbRecption) # protéger la variable
+            sp->nb_recepteurs[caseALire] += 1 # enregistrer cette lecture
+                    if sp->nb_recepteurs[caseALire] == NR: # si tous les récepteurs ont lu la case
+                            sp->nb_recepteurs[caseALire] = 0 # réinitialise le compteur
+                            V(S_EMETTEUR) # et ouvrir le droit pour écrire dans cette case
+            V(S_NbRecption) # terminer la protection pour le compteur
+            caseALire = (caseALire + 1) % NMAX # déplacer vers la prochaine case à lire
+        ```
+        côté emetteur:
+        ```python3
+            P(S_IndexEcrireCase) # protéger l'indice
+                int tmp = sp->indexEcrireCase # noter cette indice
+                sp->indexEcrireCase = (sp->indexEcrireCase+1) % NMAX # déplacer cette indice vers la prochaine case
+            V(S_IndexEcrireCase) # libérer la protection
+            P(S_EMETTEUR) # demander le droit d'écriture
+            P(S_CASES[tmp]) # protéger le contenu de la case
+                ecrire(tmp) # écrire dans la case
+            V(S_CASES[tmp]); # libérer la protection
+            for (int i = 0; i < NR; i++): # pour chaque récepteur
+                V(S_RECEPTEUR[i]) # on lui offre droit de lecture (une seule fois)
+        ```
+    1. voir le code
--- fondementOS/TME5-6/libipc/TabNcase.c
+++ fondementOS/TME5-6/libipc/TabNcase.c
@@ -1,99 +1,192 @@
 /* Diffusion tampon N case */
 
-  #include <stdio.h> 
-  #include <unistd.h> 
-  #include <signal.h> 
-  #include <libipc.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <libipc.h>
 
 /************************************************************/
 
-/* definition des parametres */ 
+/* definition des parametres */
 
-  #define NE          2     /*  Nombre d'emetteurs         */ 
-  #define NR          5     /*  Nombre de recepteurs       */ 
-  #define NMAX        3     /*  Taille du tampon           */ 
-
-/************************************************************/
-
-/* definition des semaphores */ 
-
-	// A completer
+#define NE 2   /*  Nombre d'emetteurs         */
+#define NR 5   /*  Nombre de recepteurs       */
+#define NMAX 3 /*  Taille du tampon           */
 
 /************************************************************/
 
-/* definition de la memoire partagee */ 
+/* definition des semaphores */
 
-	// A completer
-
-/************************************************************/
-
-/* variables globales */ 
-    int emet_pid[NE], recep_pid[NR]; 
-
-/************************************************************/
-
-/* traitement de Ctrl-C */ 
-
-  void handle_sigint(int sig) 
-  { int i;
-  	for (i = 0; i < NE; i++) kill(emet_pid[i], SIGKILL); 
-	for (i = 0; i < NR; i++) kill(recep_pid[i], SIGKILL); 
-	det_sem(); 
-	det_shm((char *)sp); 
- 
-  } 
+#define S_EMETTEUR 0
+#define S_IndexEcrireCase 1
+#define S_NbRecption 2
+int S_CASES[NMAX];
+int S_RECEPTEUR[NR];
 
 /************************************************************/
 
-/* fonction EMETTEUR */ 
+/* definition de la memoire partagee */
 
-	// A completer - contient les instructions executees
-        // par un emetteur
+typedef struct
+{
+        int indexEcrireCase;
+        int nb_recepteurs[NMAX];
+        int caseContenu[NMAX];
+
+} t_segpart;
+
+t_segpart *sp;
 
 /************************************************************/
 
-/* fonction RECEPTEUR */ 
-
-	// A completer - contient les instructions executees
-        // par un recepteur
+/* variables globales */
+int emet_pid[NE], recep_pid[NR];
 
 /************************************************************/
 
-int main() { 
-    struct sigaction action;
-    /* autres variables (a completer) */
-    
-    setbuf(stdout, NULL);
+/* traitement de Ctrl-C */
 
-/* Creation du segment de memoire partagee */
+void handle_sigint(int sig)
+{
+        int i;
+        for (i = 0; i < NE; i++)
+                kill(emet_pid[i], SIGKILL);
+        for (i = 0; i < NR; i++)
+                kill(recep_pid[i], SIGKILL);
+        det_sem();
+        det_shm((char *)sp);
+}
 
-	// A completer
+/************************************************************/
 
-/* creation des semaphores */ 
+/* fonction EMETTEUR */
 
-	// A completer
+// A completer - contient les instructions executees
+// par un emetteur
 
-/* initialisation des semaphores */ 
-    
-	// A completer
-    
-/* creation des processus emetteurs */ 
+int emetteur(int id_emet)
+{
+        while (1)
+        {
+                printf("Emetteur %d\n", id_emet);
 
-	// A completer - les pid des processus crees doivent
+                P(S_IndexEcrireCase);
+                        int tmp = sp->indexEcrireCase;
+                        sp->indexEcrireCase = (sp->indexEcrireCase+1) % NMAX;
+                V(S_IndexEcrireCase);
+
+                P(S_EMETTEUR);
+                
+                P(S_CASES[tmp]);
+                        sp->caseContenu[tmp] = id_emet;
+                V(S_CASES[tmp]);
+                for (int i = 0; i < NR; i++)
+                {
+                        V(S_RECEPTEUR[i]);
+                }
+        }
+        return 0;
+}
+
+/************************************************************/
+
+/* fonction RECEPTEUR */
+
+int recepteur(int id_recp)
+{
+        int caseALire = 0;
+        while (1)
+        {
+                P(S_RECEPTEUR[id_recp]);
+                P(S_CASES[caseALire]);
+                        printf("rec%d, case%d: %d \n", id_recp, caseALire, sp->caseContenu[caseALire]);
+                V(S_CASES[caseALire]);
+                
+                P(S_NbRecption);
+                        if (++sp->nb_recepteurs[caseALire] == NR)
+                        {
+                                sp->nb_recepteurs[caseALire] = 0;
+                                V(S_EMETTEUR);
+                        }
+                V(S_NbRecption);
+                caseALire = (caseALire + 1) % NMAX;
+        }
+}
+/************************************************************/
+
+int main()
+{
+        struct sigaction action;
+        /* autres variables (a completer) */
+
+        setbuf(stdout, NULL);
+
+        /* Creation du segment de memoire partagee */
+        if ((sp = (t_segpart *)init_shm(sizeof(t_segpart))) == NULL)
+        {
+                perror("init_shm");
+                exit(1);
+        }
+        
+        sp->indexEcrireCase = 0;
+        for (int i = 0; i < NMAX; i++)
+        {
+                sp->caseContenu[i] = -1;
+                sp->nb_recepteurs[i] = 0;
+        }
+
+        /* creation des semaphores */
+
+        if (creer_sem(NMAX + NR + 3) == -1)
+        {
+                perror("creer_sem");
+                exit(1);
+        }
+
+        /* initialisation des semaphores */
+
+        init_un_sem(S_EMETTEUR, NMAX);
+        init_un_sem(S_IndexEcrireCase, 1);
+        init_un_sem(S_NbRecption, 1);
+        for (int i = 0; i < NMAX; i++)
+        {
+                S_CASES[i] = i + 3;
+                init_un_sem(S_CASES[i], 1);
+        }
+        for (int i = 0; i < NR; i++)
+        {
+                S_RECEPTEUR[i] = i + 3 + NMAX;
+                init_un_sem(S_RECEPTEUR[i], 0);
+        }
+
+        /* creation des processus emetteurs */
+
+        // A completer - les pid des processus crees doivent
         // etre stockes dans le tableau emet_pid
+        for (int i = 0; i < NE; i++)
+        {
+                if ((emet_pid[i] = fork()) == 0)
+                {
+                        emetteur(i);
+                }
+        }
 
-/* creation des processus recepteurs */ 
+        /* creation des processus recepteurs */
+        for (int i = 0; i < NR; i++)
+        {
+                
+                if ((recep_pid[i]=fork()) == 0)
+                {
+                        recepteur(i);
+                }
+        }
 
-	// A completer - les pid des processus crees doivent
-        // etre stockes dans le tableau recep_pid
-    
-/* redefinition du traitement de Ctrl-C pour arreter le programme */ 
+        /* redefinition du traitement de Ctrl-C pour arreter le programme */
+        sigemptyset(&action.sa_mask);
+        action.sa_flags = 0;
+        action.sa_handler = handle_sigint;
+        sigaction(SIGINT, &action, 0);
 
-    sigemptyset(&action.sa_mask);
-    action.sa_flags = 0;
-    action.sa_handler = handle_sigint;
-    sigaction(SIGINT, &action, 0); 
-
-    pause();                     /* attente du Ctrl-C */
-    return EXIT_SUCCESS;
-} 
+        pause(); /* attente du Ctrl-C */
+        return EXIT_SUCCESS;
+}
