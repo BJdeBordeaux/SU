260a22a97ada58969d61146d5d1fe93e299cbadc
BJdeBordeaux <qq870185814@163.com>
2022-04-02 12:56:45 +0200

    tme update

---
+++ Bio-info/week7/test.py
@@ -0,0 +1,14 @@
+import random
+import numpy as np
+
+nuc = ('A', 'C', 'G', 'T')
+
+k=8 #taille de motif
+v=0 #nb de positions variables dans le motif
+t=5 #nb de sequences
+n=100 #longuer des sequence
+
+c = "c"
+c = c[:0] + "b" + c[0:]
+print(c)
+
